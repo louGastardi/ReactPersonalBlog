024a5276890ff678b33be69d45b06df147202eb7
Lou Gastardi <l.gastardi@hotmail.com>
2024-02-16 15:26:12 +0100

    links update

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
-export default function Card({ id, title }) {
+export default function Card({ post }) {
   return (
-    <div className="post">
-      <img className="cover-card" src={`/assets/posts/${id}/cover.png`} alt="post cover" />
-      <h2 className="title-card">{title}</h2>
-    </div>
+    <Link to={`${post.link}`}>
+      <div className="post">
+        <img className="cover-card" src={`/assets/posts/${post.id}/cover.png`} alt="post cover" />
+        <h2 className="title-card">{post.title}</h2>
+        <p className="p-card">{post.subtitle}</p>
+      </div>
+    </Link>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,16 @@
 import './styles.css';
+import posts from 'json/posts.json';
 import Card from 'components/Card';
 
 export default function Home() {
   return (
     <>
       <ul className="posts">
-        <li>
-          <a href="https://vimeo.com/913695583">
-            <Card id={1} title={'My Motion Design Showreel'} />
-          </a>
-        </li>
-        <li>
-          <a href="https://reframe-4030c.web.app/">
-            <Card id={2} title={'ReFrame Gratitude App | React & Node.JS REST API'} />
-          </a>
-        </li>
-        <li>
-          <a href="https://magnificent-lamington-765978.netlify.app/">
-            <Card id={3} title={'RPG Game | JS Vanilla'} />
-          </a>
-        </li>
-        <li>
-          <a href="https://movie-watchers.onrender.com/login">
-            <Card id={4} title={'Movies Watchers app | Backend REST API'} />
-          </a>
-        </li>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Card post={post} />
+          </li>
+        ))}
       </ul>
     </>
   );
