131377c86b835c78ab4078786c02d48a0457b78d
Lou Gastardi <l.gastardi@hotmail.com>
2024-02-16 14:56:51 +0100

    links to portfolio working

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,10 @@
-import { Link } from 'react-router-dom';
 import './styles.css';
-import BtnMain from 'components/BtnMain';
-export default function Card({ post }) {
+
+export default function Card({ id, title }) {
   return (
-    <Link to={`/posts/${post.id}`}>
-      <div className="post">
-        <img className="cover-card" src={`/assets/posts/${post.id}/cover.png`} alt="post cover" />
-        <h2 className="title-card">{post.title}</h2>
-        <BtnMain>Read More</BtnMain>
-      </div>
-    </Link>
+    <div className="post">
+      <img className="cover-card" src={`/assets/posts/${id}/cover.png`} alt="post cover" />
+      <h2 className="title-card">{title}</h2>
+    </div>
   );
 }
